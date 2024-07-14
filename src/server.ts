c0f8021b3424cdc9fc8f592cdda3ff8beb163f21
Mário Lucas <mariolucasdev@gmail.com>
2024-07-14 09:54:24 -0300

    adding route to update trip

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import cors from "@fastify/cors";
-import fastify from "fastify";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { confirmParticipant } from "./routes/confirm-participant";
-import { confirmTrip } from "./routes/confirm-trip";
-import { createAvticity } from "./routes/create-activity";
-import { createLink } from "./routes/create-link";
-import { createTrip } from "./routes/create-trip";
-import { getActivities } from "./routes/get-activities";
-import { getLinks } from "./routes/get-links";
-import { getParticipants } from "./routes/get-participants";
-import { createInvite } from "./routes/create-invite";
+import cors from "@fastify/cors"
+import fastify from "fastify"
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
+import { confirmParticipant } from "./routes/confirm-participant"
+import { confirmTrip } from "./routes/confirm-trip"
+import { createAvticity } from "./routes/create-activity"
+import { createLink } from "./routes/create-link"
+import { createTrip } from "./routes/create-trip"
+import { getActivities } from "./routes/get-activities"
+import { getLinks } from "./routes/get-links"
+import { getParticipants } from "./routes/get-participants"
+import { createInvite } from "./routes/create-invite"
+import { updateTrip } from "./routes/update-trip"
 
 const app = fastify();
 
@@ -18,13 +19,14 @@ app.register(cors, {
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
 })
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 /* trips routes */
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipant)
+app.register(updateTrip)
 
 /* activities routes */
 app.register(createAvticity)
@@ -39,5 +41,5 @@ app.register(getParticipants)
 app.register(createInvite)
 
 app.listen({ port: 3333}).then(() => {
-    console.log("Server running on port 3333");
+    console.log("Server running on port 3333")
 })
