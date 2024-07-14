a8ee968ac9257969847b7ca7639d3538bf628e43
Mário Lucas <mariolucasdev@gmail.com>
2024-07-14 09:10:58 -0300

    adding activity routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import cors from "@fastify/cors";
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { confirmParticipant } from "./routes/confirm.participant";
-import { confirmTrip } from "./routes/confirm.trip";
+import { confirmParticipant } from "./routes/confirm-participant";
+import { confirmTrip } from "./routes/confirm-trip";
+import { createAvticity } from "./routes/create-activity";
 import { createTrip } from "./routes/create-trip";
+import { getActivities } from "./routes/get-activities";
 
 const app = fastify();
 
@@ -15,9 +17,14 @@ app.register(cors, {
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+/* trips routes */
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipant)
+
+/* activities routes */
+app.register(createAvticity)
+app.register(getActivities)
 
 app.listen({ port: 3333}).then(() => {
     console.log("Server running on port 3333");
